d57e74dc45d74538efdcc413ffbab636dd954e7d
SrihariRamya <hariramya1994@gmail.com>
2024-01-22 19:26:42 +0530

    Added .gitignore file

--- config-overrides.js
+++
@@ -1,9 +0,0 @@
-const path = require('path');
-
-module.exports = function override(config, env) {
-  config.output.path = path.resolve(__dirname, 'build');
-  config.output.filename = 'static/js/[name].js';
-  config.output.chunkFilename = 'static/js/[name].js'
-
-  return config;
-};
