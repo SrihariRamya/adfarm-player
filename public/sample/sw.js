a1be610d8db696de55fcb0e6dee9b406ee3a7fd9
SrihariRamya <hariramya1994@gmail.com>
2024-01-29 19:51:46 +0530

    Version 2.9 to 3.0

--- public/sample/sw.js
+++ public/sample/sw.js
@@ -1,5 +1,5 @@
 
-var cache_ver = 2.9;
+var cache_ver = 3.0;
 var cache_name = 'adfarm' + cache_ver;
 self.addEventListener('install', function (event) {
   console.log('cache_name', cache_name, caches);
