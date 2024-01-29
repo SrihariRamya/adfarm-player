0e7f06e38b9594b71271ccc48a1303d6d553357d
SrihariRamya <hariramya1994@gmail.com>
2024-01-29 15:50:07 +0530

    created sample folder in pubic

--- public/sample/sw.js
+++ public/sample/sw.js
@@ -1,5 +1,5 @@
 
-var cache_ver = 1.9;
+var cache_ver = 2.0;
 var cache_name = 'adfarm' + cache_ver;
 self.addEventListener('install', function (event) {
   console.log('cache_name', cache_name, caches);
