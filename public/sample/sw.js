6c973dd8f5a915acc3113763d7cd8355acfd20b6
SrihariRamya <hariramya1994@gmail.com>
2024-01-30 12:59:11 +0530

    Verion 3.2 to 3.3

--- public/sample/sw.js
+++ public/sample/sw.js
@@ -1,5 +1,5 @@
 
-var cache_ver = 3.2;
+var cache_ver = 3.3;
 var cache_name = 'adfarm' + cache_ver;
 self.addEventListener('install', function (event) {
   console.log('cache_name', cache_name, caches);
