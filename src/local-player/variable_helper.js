75603230db6448a2a0ec4ddffbc3162739125ba6
SrihariRamya <hariramya1994@gmail.com>
2024-01-30 17:11:19 +0530

    Version 3.5 to 3.6

--- src/local-player/variable_helper.js
+++ src/local-player/variable_helper.js
@@ -1,5 +1,3 @@
-import _ from "lodash";
-
 export const FULLSCREEN = 4;
 export const BASE = 1;
 export const BASESTRIP = 2;
@@ -7,15 +5,15 @@ export const BASELEFT = 3;
 export const BASERIGHT = 5;
 export const STREAM = 2;
 export const RSS = "RSS";
-export const CACHE_VERSION = 3.5;
-export const PLAYER_ID = getPlayerData(true);
-export const IS_BROWSER = getPlayerData();
-export const TENANT = window.location.href.split("/")[2].split(".")[1];
+export const CACHE_VERSION = 3.6;
+// export const PLAYER_ID = getPlayerData(true);
+// export const IS_BROWSER = getPlayerData();
+// export const TENANT = window.location.href.split("/")[2].split(".")[1];
 
-const getPlayerData = (isPlayerId) => {
-    const queryParams = _.split(window.location.search, '&');
-    const browser = queryParams.length > 1 && queryParams[1].replace('isBrowser=', '');
-    const isBrowser = browser && browser === "true" ? true : false;
-    const player_id = Number(queryParams[0].replace('?player_id=', ''))
-    return isPlayerId ? player_id : isBrowser;
-}
+// const getPlayerData = (isPlayerId) => {
+//     const queryParams = _.split(window.location.search, '&');
+//     const browser = queryParams.length > 1 && queryParams[1].replace('isBrowser=', '');
+//     const isBrowser = browser && browser === "true" ? true : false;
+//     const player_id = Number(queryParams[0].replace('?player_id=', ''))
+//     return isPlayerId ? player_id : isBrowser;
+// }
