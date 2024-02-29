2322fe02a4221a50b070691fccb72f35caae8da0
SrihariRamya <hariramya1994@gmail.com>
2024-02-29 19:15:29 +0530

    WS issue 4.2. to 4.3

--- public/sw.js
+++ public/sw.js
@@ -1,5 +1,5 @@
 
-var cache_ver = 4.2;
+var cache_ver = 4.3;
 var cache_name = 'adfarm' + cache_ver;
 self.addEventListener('install', function (event) {
   console.log('cache_name', cache_name, caches);
@@ -80,11 +80,11 @@ self.addEventListener('activate', function (event) {
       ).then(() => {
         console.log('about to reload in New code');
         // Reload the page
-        self.clients.matchAll().then(function (clients) {
-          clients.forEach(function (client) {
-            client.navigate(client.url);
-          });
-        });
+        // self.clients.matchAll().then(function (clients) {
+        //   clients.forEach(function (client) {
+        //     client.navigate(client.url);
+        //   });
+        // });
       });
     })
   );
