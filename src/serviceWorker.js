eca2f542c7bd2feff3565d756a053dadc0b1adfc
SrihariRamya <hariramya1994@gmail.com>
2024-03-06 18:56:22 +0530

    WS 5.8 to 5.9

--- src/serviceWorker.js
+++ src/serviceWorker.js
@@ -3,7 +3,7 @@ export function register(isAppCrashed) {
     navigator.serviceWorker.getRegistrations().then(registrations => {
       if (registrations.length == 0) {
         console.log('Registration called in ServiceWorker')
-        navigator.serviceWorker.register('swv9.js')
+        navigator.serviceWorker.register('swv2.js')
           .then(function (registration) {
             var serviceWorker;
             if (registration.installing) {
@@ -39,8 +39,8 @@ export function register(isAppCrashed) {
         console.log('registrations', registrations, '<<>>')
         const needUnregister = window.localStorage.getItem("registeredFile");
         console.log('needUnregister', needUnregister, '<<>>')
-        if (isAppCrashed || (needUnregister !== "swv9")) {
-          localStorage.setItem("registeredFile", "swv9");
+        if (isAppCrashed || (needUnregister !== "swv2")) {
+          localStorage.setItem("registeredFile", "swv2");
           registrations[0].unregister().then(function(success) {
             setTimeout(() => {
               console.log('Reload called ServiceWorker');
